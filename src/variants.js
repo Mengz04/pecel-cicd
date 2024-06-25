219c6099b7e7895e6e967e09a059439d319496ef
Muhammad Rafi Insan Fillah <rafifillah@gmail.com>
2024-06-25 22:36:03 +0700

    feat: base project

--- src/variants.js
+++ src/variants.js
@@ -4,8 +4,8 @@ export default function Variants(props){
     return(
         <div className={`w-1/3 aspect-square flex flex-col justify-center items-center bg-slate-50 drop-shadow-lg rounded-lg transition duration-500 hover:scale-105 hover:shadow-md ${props.color} ${props.bgchange}`}>
             <div className='flex flex-row w-full justify-center items-center'>
-                <img src={props.image1} className='w-5 h-5 lg:w-8 lg:h-8' alt=' '/>
-                <img src={props.image2} className='w-5 h-5 lg:w-8 lg:h-8' alt=' '/>
+                <img src={props.image1} className='w-5 h-5 lg:w-8 lg:h-8' alt='propsimg1'/>
+                <img src={props.image2} className='w-5 h-5 lg:w-8 lg:h-8' alt='propsimg1'/>
             </div>
             <p className='text-center mt-3 text-sm lg:text-base'>{props.kategori}</p>
         </div>
