67a82d7b1a3cf15d0a66f31d7c618ad07b84955e
Muhammad Rafi Insan Fillah <rafifillah@gmail.com>
2024-06-25 22:20:53 +0700

    feat: base project

--- src/banner.js
+++ src/banner.js
@@ -7,9 +7,9 @@ export default function Banner(){
     return(
         <div className='flex relative w-full lg:h-[600px] h-auto overflow-hidden'>
             <img src={BannerBG} className='object-cover w-full h-auto z-0'/>
-            <img src={Gulajawa} className='absolute z-10 -bottom-3 lg:-bottom-5 left-5 lg:left-40 w-1/4'/>
-            <img src={Cabai} className='absolute z-10 m-auto inset-x-0 -bottom-1 lg:bottom-[5px] w-1/3'/>
-            <img src={Kacang} className='absolute z-10 -bottom-2 right-5 lg:right-40 w-1/4'/>
+            <img src={Gulajawa} className='absolute z-10 -bottom-3 lg:-bottom-5 left-5 lg:left-40 w-1/4' alt="image"/>
+            <img src={Cabai} className='absolute z-10 m-auto inset-x-0 -bottom-1 lg:bottom-[5px] w-1/3' alt="image"/>
+            <img src={Kacang} className='absolute z-10 -bottom-2 right-5 lg:right-40 w-1/4' alt="image"/>
             <div className='absolute z-20 inset-0 m-auto w-full h-full'>
                 <div className='flex flex-col justify-center items-center text-center w-full h-3/4 lg:h-1/2'>
                     <h3 className='banner-header-top text-lg sm:text-3xl lg:text-5xl'>made with</h3>
